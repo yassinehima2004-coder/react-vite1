08503a40e2dc3cd56ba5914113a5f78482a42092
yassinehima2004-coder <yassinehima2004@gmail.com>
2026-04-06 14:09:48 +0100

    v1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+
+
+function App() {
+  return (
+    <div className='bg-green-500 text-white p-10 text-center'>
+      Tailwind fonctionne 🚀
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+        <h1>CopyWrite Yassine</h1>
+    </footer>
+  );
+}
+
+export default Footer;
